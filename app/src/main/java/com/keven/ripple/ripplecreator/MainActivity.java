05519a9d4336d7ad949459bb4d28e86e285d6963
keven1119 <kid.worldkeven@gmail.com>
2017-11-21 11:21:57 +0800

    first_commit

---
+++ app/src/main/java/com/keven/ripple/ripplecreator/MainActivity.java
@@ -0,0 +1,42 @@
+package com.keven.ripple.ripplecreator;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.keven.ripple.ripplecreator.helper.JoyrunRipple;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageview_main);
+
+        Drawable build = new JoyrunRipple.Builder(this)
+                .setEndColor(R.color.red_color)
+                .setStartColor(R.color.green_color)
+                .setNormalColor(R.color.blue_color)
+                .setPressColor(R.color.red_color)
+                .setRippleColor(R.color.pink_color)
+                .setStrokeColor(R.color.blue_color)
+                .setStrokeWidth(10)
+                .setRadius(30).build();
+
+        imageView.setImageDrawable(build);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+}
